adb918ded09dc26a562764f3ab8ca219d9e21874
Lucasz-py <escobarlucas27.10@gmail.com>
2026-05-15 17:04:34 -0300

    agregue UsuariosSeeder fix

--- database/seeders/UsuariosSeeder.php
+++ database/seeders/UsuariosSeeder.php
@@ -13,20 +13,6 @@ class UsuariosSeeder extends Seeder
         $rolAdmin   = Rol::where('nombre', 'admin')->first();
         $rolCliente = Rol::where('nombre', 'cliente')->first();
 
-        $usuarios = [
-            [
-                'nombre'   => 'Administrador',
-                'email'    => 'admin@gamingstore.com',
-                'password' => 'admin1234',
-                'rol_id'   => $rolAdmin->id,
-            ],
-            [
-                'nombre'   => 'Cliente Demo',
-                'email'    => 'cliente@gamingstore.com',
-                'password' => 'cliente1234',
-                'rol_id'   => $rolCliente->id,
-            ],
-        ];
 
         foreach ($usuarios as $datos) {
             // firstOrCreate evita duplicados si se ejecuta más de una vez
